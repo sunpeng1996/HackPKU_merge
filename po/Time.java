2c9c5dae4de53a0bc2ba435651a69c5bcfeddb85
孙鹏 <287594495@qq.com>
2016-04-09 09:20:41 +0800

    ok

---
+++ po/Time.java
@@ -0,0 +1,84 @@
+package hit.po;
+
+public class Time {
+    private Integer timeId;
+
+    private Integer year;
+
+    private Integer month;
+
+    private Integer week;
+
+    private Integer day;
+
+    private Integer class_;
+
+    private Integer hour;
+
+    private Integer minute;
+
+    public Integer getTimeId() {
+        return timeId;
+    }
+
+    public void setTimeId(Integer timeId) {
+        this.timeId = timeId;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public Integer getWeek() {
+        return week;
+    }
+
+    public void setWeek(Integer week) {
+        this.week = week;
+    }
+
+    public Integer getDay() {
+        return day;
+    }
+
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+
+
+    public Integer getClass_() {
+		return class_;
+	}
+
+	public void setClass_(Integer class_) {
+		this.class_ = class_;
+	}
+
+	public Integer getHour() {
+        return hour;
+    }
+
+    public void setHour(Integer hour) {
+        this.hour = hour;
+    }
+
+    public Integer getMinute() {
+        return minute;
+    }
+
+    public void setMinute(Integer minute) {
+        this.minute = minute;
+    }
+}
